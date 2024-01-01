ba549659f89e45bf6c4187046144feb857e8d46b
Ahmed safwat <a01273477051@gmail.com>
2024-01-01 22:03:56 +0200

    done with the main structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  Footer,
+  Blog,
+  Feature,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./container";
+import { CTA, Brand, Navbar } from "./components";
+import "./app.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
---
+++ src/container/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,7 @@
+import './whatGPT3.css'
+
+const WhatGPT3 = () => {
+    return ( <div>What</div> );
+}
+ 
+export default WhatGPT3;
